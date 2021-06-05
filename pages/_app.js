3642ab9dcf139057d3c3c64c8cf8350b7674ed2a
Karthikeyan Umapathi <karthikeyan90422@gmail.com>
2021-06-05 22:17:42 +0530

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,19 @@ export default function App({ Component, pageProps }) {
     <ThemeProvider attribute="class">
       <MDXProvider components={MDXComponents}>
         <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `<!-- Global site tag (gtag.js) - Google Analytics -->
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-WMX95SBGPE"></script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', 'G-WMX95SBGPE');
+              </script>`,
+            }}
+          />
           <meta content="width=device-width, initial-scale=1" name="viewport" />
         </Head>
         <DefaultSeo {...SEO} />
